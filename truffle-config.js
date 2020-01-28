525e471eacf103b5d505367b3856bf6b58d75392
SimpleTurningMove01 <46304492+SimpleTurningMove01@users.noreply.github.com>
2020-01-27 20:03:53 -0500

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,13 @@
+// 01/27/2020-jd 
+module.exports = {
+//   compilers: {
+//     solc: '0.4.25'
+//   },
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
